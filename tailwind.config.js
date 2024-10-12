8fef8b8224210ff775b9198ca3914f1129c27779
yaxyobekb <pubgn9642@gmail.com>
2024-10-12 15:37:28 +0500

    add some tailwind config styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,30 @@ export default {
         secondary: "#478CCF",
         accent: "#36C2CE",
         lightAccent: "#77E4C8",
-        background: "#F6FFFF",
+        background: "#F2F5FC",
+        dark: {
+          800: "#333333",
+        },
       },
     },
+
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+
+    boxShadow: {
+      sm: "0 0 10px 0 rgba(219,227,227,0.7)",
+    },
+
+    backgroundImage: {
+      "gradient-orange": "linear-gradient(120deg, #f6d365 0%, #fda085 100%)",
+      "gradient-to-b": "linear-gradient(to bottom, var(--tw-gradient-stops))",
+    },
+
+    screens: {
+      xs: "400px",
+    },
   },
+
   plugins: [],
 };
