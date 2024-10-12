e5381a81de171c4b99b712ff88a584e600ce4283
yaxyobekb <pubgn9642@gmail.com>
2024-10-12 15:36:58 +0500

    add emojis data

---
+++ src/data/emojis.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Beaming%20Face%20With%20Smiling%20Eyes.webp",
+    name: "Beaming Face With Smiling Eyes",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Alien%20Monster.webp",
+    name: "Alien Monster",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Cat%20With%20Wry%20Smile.webp",
+    name: "Cat With Wry Smile",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Face%20With%20Peeking%20Eye.webp",
+    name: "Face With Peeking Eye",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Ghost.webp",
+    name: "Ghost",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Grinning%20Cat%20With%20Smiling%20Eyes.webp",
+    name: "Grinning Cat With Smiling Eyes",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Grinning%20Face.webp",
+    name: "Grinning Face",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Grinning%20Face%20With%20Sweat.webp",
+    name: "Grinning Face With Sweat",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Saluting%20Face.webp",
+    name: "Saluting Face",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Melting%20Face.webp",
+    name: "Melting Face",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Smiling%20Face%20With%20Smiling%20Eyes.webp",
+    name: "Smiling Face With Smiling Eyes",
+  },
+  {
+    src: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Smileys/Upside%20Down%20Face.webp",
+    name: "Upside Down Face",
+  },
+];
